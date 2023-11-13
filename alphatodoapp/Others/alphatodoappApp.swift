9d53cffb39af684a5de20f3cf8af09dae90884d4
Eyüp Furkan Tüylü <eyupfurkantuylu@gmail.com>
2023-11-13 20:20:18 +0300

    Firebase complated
    - firebase complated

--- alphatodoapp/Others/alphatodoappApp.swift
+++ alphatodoapp/Others/alphatodoappApp.swift
@@ -4,11 +4,14 @@
 //
 //  Created by Eyüp Tüylü on 13.11.2023.
 //
-
+import FirebaseCore
 import SwiftUI
 
 @main
 struct alphatodoappApp: App {
+    init(){
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             MainView()
